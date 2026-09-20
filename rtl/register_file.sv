88c4106281bedb331aa2461f95f6d1249109da0f
1x31744 <koopahello@gmail.com>
2026-09-20 14:57:45 +0100

    finished register file i think

--- rtl/register_file.sv
+++ rtl/register_file.sv
@@ -2,10 +2,13 @@ module register_file (
     input logic clk,
     input logic reset,
     input logic write_enable,
-    input logic [5:0] write_address,
+    input logic [4:0] write_address,
     input logic [31:0] write_data,
-    input logic [5:0] read_address,
-    output logic [31:0] read_data
+
+    input logic [4:0] read_address_a,
+    output logic [31:0] read_data_a,
+    input logic [4:0] read_address_b,
+    output logic [31:0] read_data_b
 );
 // 32 = program counter
 /* Registers x0-x31
@@ -13,6 +16,23 @@ module register_file (
 * standard calling also uses x2 as the stack pointer
 */
 logic [31:0] regs [0:31];
+
+always_ff @(posedge clk) begin
+    if (reset) begin
+        for (int i = 0; i < 32; i++) begin
+            regs[i] <= 32'b0;
+        end
+    end
+    else if (write_enable && write_address != 0) begin
+        regs[write_address] <= write_data;
+    end
+end
+
+always_comb begin
+    read_data_a = regs[read_address_a];
+    read_data_b = regs[read_address_b];
+end
+
 
 
 endmodule
