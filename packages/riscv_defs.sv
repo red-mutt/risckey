6c85e954fa142eccbbe9da7357cd5f3d14a0f85c
1x31744 <koopahello@gmail.com>
2026-09-24 00:18:02 +0100

    started branching decoding stuff

--- packages/riscv_defs.sv
+++ packages/riscv_defs.sv
@@ -37,17 +37,46 @@ package riscv_defs;
         F3_SRL_SRA = 2'h5,
         F3_SLT = 2'h2,
         F3_SLTU = 2'h3
-    } funct3_r_t
+    } funct3_r_t;
+
+    typedef enum logic [1:0] {
+        LOAD_STORE_BYTE = 2'h0,
+        LOAD_STORE_HALF = 2'h1,
+        LOAD_STORE_WORD = 2'h2,
+        LOAD_BYTEU = 2'h4,
+        LOAD_HALFU = 2'h5
+    } funct3_mem_t;
 
     typedef enum logic [7:0] {
         F7_ADD = 8'h00,
         F7_SUB = 8'h20
-    } funct7_r_add_sub
+    } funct7_r_add_sub;
 
     typedef enum logic [7:0] {
         F7_SRL = 8'h00,
-        F7_SRA = 8'20
-    } funct7_r_shifts
+        F7_SRA = 8'h20
+    } funct7_r_shifts;
+
+    typedef enum logic [1:0] {
+        SOURCE_ALU = 2'b01,
+        SOURCE_MEM = 2'b10,
+        SOURCE_REG = 2'b11
+    } write_source_t;
+
+    typedef enum logic [1:0] {
+        SIZE_BYTE = 2'b01,
+        SIZE_HALF = 2'b10,
+        SIZE_WORD = 2'b11
+    } mem_size_t;
+
+    typedef enum logic [2:0] {
+        BRANCH_EQ = 3'b001,
+        BRANCH_NE = 3'b010,
+        BRANCH_LT = 3'b100,
+        BRANCH_GE = 3'b011,
+        BRANCH_LTU = 3'b110,
+        BRANCH_GEU = 3'b111 
+    } branch_t
 
 
 
